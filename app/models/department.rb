7d3383406c02c4f0d770f2a5a6f8e44520e1d7fa
Ahmed Refaat Mohamed <ahmed.refaat532@gmail.com>
2019-07-29 22:52:24 +0200

    Model validations

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,3 +1,5 @@
 class Department < ApplicationRecord
   has_many :staffs
+  validates :name, presence: true, uniqueness: true  
 end
+
--- app/models/staff.rb
+++ app/models/staff.rb
@@ -2,12 +2,30 @@ class Staff < ApplicationRecord
   belongs_to :department
   has_secure_password
 
-  enum role: [:user, :admin]
-  after_initialize :set_default_role, :if => :new_record?
+  validates :name,
+            presence: true
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :base_salary,
+            presence: true,
+            numericality: true # Probably should make it greater than 0 (taking money from staff?)
+  validates :monthly_bonus,
+            numericality: { greater_than_or_equal_to: 0,
+                            less_than_or_equal_to: 1}
+  validates :department,
+            presence: true
 
-  def set_default_role
+  after_initialize :init
+
+  def init
+    self.monthly_bonus ||= 0.1
     self.role ||= :user
   end
+            
+
+  enum role: [:user, :admin]
   
   def self.salaries_total
     self.sum('base_salary')
