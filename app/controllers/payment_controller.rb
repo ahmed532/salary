3a56b87a3c4d8218e6d2079a8e642e19bfd70bc5
Ahmed Refaat Mohamed <ahmed.refaat532@gmail.com>
2019-07-29 23:25:45 +0200

    Filter by month endpoint

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -1,6 +1,6 @@
 class PaymentController < ApplicationController
 
-  let :admin, :summary
+  let :admin, :summary, :month_details
   let :all, :error
   
   def summary
@@ -9,6 +9,18 @@ class PaymentController < ApplicationController
     (current_month..12).each do |m|
       @months << MonthSummary.new(m)
     end
+  end
+
+  def month_details
+    begin
+      month_num = Integer(params[:month_num])
+    rescue
+      return render json: { error: 'Bad month number' }, status: 400
+    end
+    if not month_num.between?(1, 12)
+      return render json: { error: 'Bad month number' }, status: 400
+    end
+    @month = MonthSummary.new(month_num)
   end
   
   def error
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'payment/summary'
   get 'payment/error'
+  get 'payment/month_details/:month_num', to: 'payment#month_details'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'authenticate', to: 'authentication#authenticate'
+  resources :departments
+  resources :staff
 end
 
